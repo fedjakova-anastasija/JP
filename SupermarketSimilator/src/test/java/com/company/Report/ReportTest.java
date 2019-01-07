1a7e7fecb92f7c00bee9bfedfc4c2a66babb2eb0
Федякова Анастасия <fedyakova.anastasia@gmail.com>
2019-01-07 22:06:19 +0300

    Добавлен тест в отчет.

--- SupermarketSimilator/src/test/java/com/company/Report/ReportTest.java
+++ SupermarketSimilator/src/test/java/com/company/Report/ReportTest.java
@@ -10,7 +10,9 @@ import java.util.List;
 
 public class ReportTest {
   private Product firstProduct = new Product("product#0", 1, 10, true);
-  private BigDecimal amount = new BigDecimal(0);
+  private Product secondProduct = new Product("product#0", 1, 10, true);
+  private Product thirdProduct = new Product("product#1", 1, 10, true);
+  private BigDecimal cashAmount = new BigDecimal(0);
   private List<Bill> billsList = new ArrayList<>();
   private Report report = new Report();
 
@@ -18,10 +20,18 @@ public class ReportTest {
   public void setBillsList() {
     List<Product> productsList = new ArrayList<>();
     productsList.add(firstProduct);
-    Bill bill = new Bill(amount, productsList);
+    productsList.add(secondProduct);
+    productsList.add(thirdProduct);
+    this.cashAmount = this.cashAmount.add((firstProduct.getPrice()).multiply(new BigDecimal(firstProduct.getCount())));
+    this.cashAmount = this.cashAmount.add((secondProduct.getPrice()).multiply(new BigDecimal(secondProduct.getCount())));
+    this.cashAmount = this.cashAmount.add((thirdProduct.getPrice()).multiply(new BigDecimal(thirdProduct.getCount())));
+    Bill bill = new Bill(cashAmount, productsList);
     billsList.add(bill);
     report.setBillsList(bill);
     Assert.assertEquals(billsList.size(), 1);
+    Assert.assertEquals(productsList.size(), 3);
+    Assert.assertEquals(firstProduct.getCount().intValue(), 2);
+    Assert.assertEquals(bill.getAmount().intValue(), 30);
   }
 
   @Test
